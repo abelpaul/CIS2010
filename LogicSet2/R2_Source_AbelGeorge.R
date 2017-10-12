07a05c24fb883c3c21d4a00c806b549da80bd95f
Abel <abel.paul.george@gmail.com>
2017-10-12 08:25:44 -0400

    ProblemSet2

---
+++ LogicSet2/R2_Source_AbelGeorge.R
@@ -0,0 +1,17 @@
+myname ="Abel";
+x = seq(1,10,by=2);
+print(x);
+y = seq(0,100, by=5);
+print(y);
+z= seq(100, 1, by=-5)
+print(z)
+if (b1) print ("Hello World") else print("Goodbye Sunshine");
+if (n>e) print("Hello World") else print("Goodbye Sunshine");
+if (b1) { s1<- "hello"; print(s1);} else {s1 <- "goodbye"; print(s1);}
+for (i in 1:100) print("hello world");
+print(i);
+activity = "I will not facebook ";
+place = "at the dinner table\n";
+x = seq(1, 100, by=1);
+for (i in x) cat(activity,place);
+
